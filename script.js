42f4111d83b52c0fab22adca88b6fb26572ab405
AlliSwyt <swytap22@wfu.edu>
2026-03-03 12:48:27 -0500

    Made a semi functional back arrow feature

--- script.js
+++ script.js
@@ -6,17 +6,22 @@ const startButton = document.getElementById('start-button');
 
 //Objects for the tutorial
 const tutorial = document.getElementById('tutorial');
+const backArrow = document.getElementById('master-back-arrow');
+
 const bookDropDoorZoomOut = document.getElementById('bookDropDoorZoomOutPage');
 const bookDropDoorZoomIn = document.getElementById('bookDropDoorZoomInPage')
 const bookDropDoorHitbox = document.getElementById('bookDropDoor-hitbox');
 const bookDrop = document.getElementById('bookDropClosedPage');
 const bookDropHitbox = document.getElementById('bookDrop-hitbox');
 const bookDropDoorHandleHitbox = document.getElementById("bookDropDoorHandle-hitbox");
-const bookDropDoorHandle = document.getElementById("bookDropDoorHandle");
+const bookDropDoorHandle = document.getElementById("bookDropDoorHandlePage");
+
+
+
+
 
 //function to start the tutorial: runs when the player clicks the start game button
 function playTutorial() {
-    console.log("Start button was clicked!");
 
     //FIXME add text boxes for tutorial
 
@@ -24,37 +29,49 @@ function playTutorial() {
     menu.classList.add('hidden');
     tutorial.classList.remove('hidden');
     bookDropDoorZoomOut.classList.remove('hidden');
+    backArrow.classList.remove('hidden'); //fixme currently no functionality, want it to back up to view the hallway later ? or maybe remove for this image
 
     //if they click on the door, it zooms in on it
     bookDropDoorHitbox.addEventListener('click', goToBookDropDoorZoomIn);
+    //backArrow.addEventListener('click', goTo...);
 
     function goToBookDropDoorZoomIn() {
-        console.log("Swapping to the close-up photo");
 
         //hide book drop zoomed out image
-        bookDropDoorZoomOut.classList.add('hidden');
+        hideAllTutorialPages();
 
         //show book drop zoomed in image
         bookDropDoorZoomIn.classList.remove('hidden');
+        backArrow.addEventListener('click', goToBookDropDoorZoomOut);
 
         bookDropHitbox.addEventListener('click', goToBookDrop);
-
-        function goToBookDrop() {
-            bookDropDoorZoomIn.classList.add('hidden');
-            bookDrop.classList.remove('hidden');
-        }
-
-        function goToBookDropDoorHandle() {
-            bookDropDoorZoomIn.classList.add('hidden');
-
-
-        }
+        bookDropDoorHandleHitbox.addEventListener('click', goToBookDropDoorHandle);
 
     }
 
+    function goToBookDropDoorZoomOut() {
+        hideAllTutorialPages();
+        bookDropDoorZoomOut.classList.remove('hidden');
+        bookDropHitbox.addEventListener('click', goToBookDrop);
+        bookDropDoorHandleHitbox.addEventListener('click', goToBookDropDoorHandle);
 
+    }
+    function goToBookDrop() {
+        hideAllTutorialPages();
+        bookDropDoorZoomIn.classList.add('hidden');
+        bookDrop.classList.remove('hidden');
+    }
 
+    function goToBookDropDoorHandle() {
+        hideAllTutorialPages();
+        bookDropDoorZoomIn.classList.add('hidden');
+        bookDropDoorHandle.classList.remove('hidden');
+    }
 
+    function hideAllTutorialPages() {
+        const pages = document.querySelectorAll('.fit');
+        pages.forEach(page => page.classList.add('hidden'));
+    }
 
 }
 
